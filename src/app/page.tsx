39485727800d4735f5b639b2dc3f2899c6477ac1
lookingforcharlie <charliefeng2020@gmail.com>
2024-05-15 23:28:48 -0400

    testing db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import { db } from '@/db/index'
 
-export default function Home() {
+export default async function Home() {
+  // testing is the name of the table we just created
+  const items = await db.query.testing.findMany()
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
       <h1>hello world</h1>
+      {items.map((item) => {
+        return <div key={item.id}>{item.name}</div>
+      })}
     </main>
   )
 }
