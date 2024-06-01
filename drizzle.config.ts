40b887f4d84743479ceb3748c4ee356c43b27513
lookingforcharlie <charliefeng2020@gmail.com>
2024-05-31 22:33:31 -0400

    fixed drizzleConfig bug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   schema: './src/db/schema.ts',
-  dialect: 'postgresql', // "mysql" | "sqlite" | "postgresql"
+  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
+  verbose: true,
+  strict: true,
 })
